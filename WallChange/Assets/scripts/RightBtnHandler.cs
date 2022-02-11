da46b0d83b3e76f88031fb90e94b05dcce459ece
ayush <ayushshreshthi41@gmail.com>
2022-02-11 14:29:14 +0530

    adding

---
+++ WallChange/Assets/scripts/RightBtnHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RightBtnHandler : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        PlayerController.left = false;
+        PlayerController.right = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        PlayerController.right = false;
+    }
+}
---
+++ WallChange/Assets/scripts/UiHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UiHandler : MonoBehaviour
+{
+    private void Start()
+    {
+        Invoke("ShowAd", 2.5f);
+        
+    }
+   
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void ExitBtn()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void ShowAd()
+    {
+        AdsController.VideoAd();
+    }
+}
